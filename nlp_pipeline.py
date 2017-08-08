2d3f41e0504a5634449dabc0ebb9818228f96177
Yuri Plotkin <plotkiny@gmail.com>
2017-08-07 20:20:36 -0400

    adding part-of-speech tagging and updated logic for dealing with unknown words

--- nlp_pipeline.py
+++ nlp_pipeline.py
@@ -1,29 +1,33 @@
 
-
-import sys,json, re
+import sys,json,re, string
 import numpy as np
 import pandas as pd
+import random
 import pickle
-import enchant
 from collections import Counter
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import stopwords
+from nltk import pos_tag
 
-sys.path.append("/home/paperspace/Documents/code/pipeline2/")
+sys.path.append("/home/paperspace/Documents/development/pipelineA/")
 from english_contractions import contractions
 
 #downstream dependencies
 codes = ['<unk>','<pad>','<eos>','<go>']
-english_dictionary = enchant.Dict("en_US")nglish_contractions import contractions
+
+import enchant
+english_dictionary = enchant.Dict("en_US")
+
 
 #check if json file or csv
 li=[]
 with open("/home/paperspace/Documents/data/signalmedia-1m.jsonl") as file:
-    for line in file:
+    for line in file: 
         j = json.loads(line)
         li.append(j)
+
 
 def sentence_sampling(text, count):
     
@@ -40,7 +44,7 @@ def sentence_sampling(text, count):
     return sampled_text
 
 
-def text_cleaner(text, number_to_collect = 5, sample_sentence = True, remove_stop_words=True, expand_words=True):
+def text_cleaner(text, number_to_collect = 5, sample_sentence = True, remove_stop_words=True, expand_words=True, pos=True):
     
     text = sent_tokenize(text)
     max_length = len(text)
@@ -58,32 +62,37 @@ def text_cleaner(text, number_to_collect = 5, sample_sentence = True, remove_sto
             text = sentence_sampling(text,count) #randomly sample sentences from the paragraph
         else:
             text = text[:max_length]
+            
+    #get the part-of-speech for the sentences
+    if pos:
+        translation = str.maketrans("","", string.punctuation);
+        tagged_tuples = [pos_tag(word_tokenize(sentence)) for sentence in text] #list of lists with each list containing (word, tag)
+        list_of_tags = [[item[1] for item in li] for li in tagged_tuples] #get the tag for each word, returns list of lists 
+        list_of_tags = [' '.join(li).translate(translation) for li in list_of_tags] #list of strings with tags
+        list_of_tags = [li + '<eos>' for li in list_of_tags] #add end of sentence tag to the end of each string
+        list_of_tags = ' '.join(list_of_tags).split() #ouptut is a list of strings --> join into one sentence and split on whitespace (getting rid of double white space)
+        list_of_tags = ' '.join(list_of_tags)
         
     #add end of sentence tag "<EOS>"
     text  = ['{} {}'.format(sentence,codes[2]) for sentence in text]
 
-    
     #lower case => returns a single list of words (i.e. ['today was a good day the dog ate my homework'])
     text = [x.lower() for x in text]
     text = ' '.join(text)
-    
     
     #replace contraction words with the longer form
     if expand_words:
         updated_text = []  #updated_text is a list of words
         for word in text.split():
             updated_text.append(contractions[word]) if word in contractions else updated_text.append(word)
-
-            
+       
     #lemmatization step
     lemma = WordNetLemmatizer()
     text = [lemma.lemmatize(word) for word in updated_text]
     
-    
     #check if the word is in the english dictionary
     text = [word for word in text if english_dictionary.check(word)]
     text = ' '.join(updated_text)  
-    
     
     #remove stop words
     #TODO: KEEP OR REMOVE STOP WORDS
@@ -102,6 +111,10 @@ def text_cleaner(text, number_to_collect = 5, sample_sentence = True, remove_sto
     text = re.sub(r'< eos >', '<eos>', text) # remove whitespace
     text = re.sub(r'``', '', text) #remove ``
     
+    if pos:
+        text = '{} {}'.format(text, list_of_tags) # add the word string to the part of speech string
+    
+    
     # look into adapting '6', 'p', 'm'
     # look into removing ''
     #text = re.sub(r'[0-9]{2}/[09]{2}/[0-9]{2}', '', text) #replace date 09/17/15 
@@ -115,7 +128,8 @@ def text_cleaner(text, number_to_collect = 5, sample_sentence = True, remove_sto
     # 
     
     return text
-        
+
+   
 #apply text_cleaner to the entire dataset
 remove_keys = ['id','published'] 
 processed_li = []
@@ -133,6 +147,7 @@ for sample in li:
     text_dictionary['media-type'] = text_cleaner(text_dictionary['media-type'])
     text_dictionary['source'] = text_cleaner(text_dictionary['source'])
     processed_li.append(text_dictionary)
+    
 
 #distribution of words in the corpus
 counter_dictionary = {}
@@ -147,16 +162,21 @@ for sample in processed_li:
 
 set_counter = Counter(counter_dictionary)
 
+with open('/home/paperspace/Documents/development/Experiments/with_POS_20K_vocab_unkThres.10/counter_dictionary_aug7.txt', 'wb') as output:
+    pickle.dump(counter_dictionary, output)
+
 #get embeddings list using ConceptNet
 embedding_list = {}
 def get_embeddings():
     
-    with open('/home/paperspace/Documents/data/numberbatch-en-17.04b.txt') as file:
+    with open('/home/paperspace/Documents/development/data/numberbatch-en-17.04b.txt') as file:
         for line in file:
             vector = line.split()
             word = vector[0]
             embedding = np.array(vector[1:], dtype='float32')
             embedding_list[word] = embedding
+
+
 #find the words that do not have embeddings from ConceptNet
 #Set a threshold count to be used in our vocabulary and remove words that do not meet the count
 
@@ -164,10 +184,16 @@ def get_embeddings():
 #TODO: Determine the right vocabulary size -> add a check in the prune_and_embed method 
 #to take the 25K most frequent words after the threshold check. Limiting the size of the vocabulary to somewhere there...
 
+#find the words that do not have embeddings from ConceptNet
+#Set a threshold count to be used in our vocabulary and remove words that do not meet the count
+#TODO: Determine the right vocabulary size -> add a check in the prune_and_embed method 
+#to take the 25K most frequent words after the threshold check. Limiting the size of the vocabulary to somewhere there...
+
 words_without_embeddings = []
+embed_dim = 300
 
 def prune_and_embed(set_counter, embedding_list, codes, words_without_embeddings,
-                    threshold_count = 1, embed_dim = 300, max_length = 40000):
+                    threshold_count = 1, embed_dim = 300, max_length = 20000):
     
     assert(embed_dim > 100)
     
@@ -212,16 +238,20 @@ def prune_and_embed(set_counter, embedding_list, codes, words_without_embeddings
 #TODO: verify that i even need the "words_without_embeddings" variable in the code base
 word_to_ind,ind_to_word,embed_matrix, words_without_embeddings = prune_and_embed(set_counter, embedding_list, 
                                                                                  codes, words_without_embeddings)
-            
-with open('/home/paperspace/Documents/data/word_to_ind.txt', 'wb') as output:
+
+
+with open('/path/embed_matrix-TEST_aug7.txt', 'wb') as output:
+    pickle.dump(embed_matrix, output)
+
+with open('/path/word_to_ind-TEST_aug7.txt', 'wb') as output:
     pickle.dump(word_to_ind, output)
 
-with open('/home/paperspace/Documents/data/ind_to_word.txt', 'wb') as output:
+with open('/path/ind_to_word-TEST_aug7.txt', 'wb') as output:
     pickle.dump(ind_to_word, output)
     
-
 #converting all words to integers
 #calculating the distribution of lengths for each field 
+
 processed_li_to_ind = []
 columns = ['content','media_type','source','title']
 count_dataframe = pd.DataFrame(columns=columns)
@@ -253,13 +283,6 @@ for sample in processed_li:
     dataframe = pd.DataFrame([len_values], columns=columns)
     count_dataframe = count_dataframe.append(dataframe)
 
-print(count_dataframe.describe())
-print(int(np.percentile(count_dataframe.content, 96)));
-print(np.percentile(count_dataframe.media_type, 96));
-print(np.percentile(count_dataframe.source, 96));
-print(np.percentile(count_dataframe.title, 96));
-
-
 
 #remove reviews if they have too many unknown (as a result of filtering scarce words from threshold set upstream)
 #TODO: from a sample of 1000, the average number of words is 244 (99th percentile). Think about better algorithmor implementation
@@ -288,56 +311,70 @@ def is_padding_required(v, min_len):
     
     return v
 
-def remove_reviews_and_set_padding(processed_li_to_ind, threshold = .15):
+def remove_reviews_and_set_padding(processed_li_to_ind, threshold):
+    
+    data = []
     
     #assuming a gaussian distribution, using about 2 standard deviations away from the mean
-    content_len = int(np.percentile(count_dataframe.content, 96)) 
-    media_type_len = int(np.percentile(count_dataframe.media_type, 96)) 
-    source_len = int(np.percentile(count_dataframe.source, 96)) 
-    title_len = int(np.percentile(count_dataframe.title, 96)) 
+    content_len = int(np.percentile(count_dataframe.content, 60)) 
+    media_type_len = int(np.percentile(count_dataframe.media_type, 60)) 
+    source_len = int(np.percentile(count_dataframe.source, 60)) 
+    title_len = int(np.percentile(count_dataframe.title, 60)) 
     
     #removing samples with too many unknown words
-    #index <unk> = 18093
     unknown_index = word_to_ind['<unk>']
-    unknown_threshold = int(content_len*threshold)
+    unknown_threshold_content = int(content_len*threshold)
+    unknown_threshold_title = int(title_len*threshold)
     
     for sample in processed_li_to_ind:
-        for k,v in sample.items():
         
-            v = v.split()
+        keep_content = True; keep_title = True
+        
+        for k,v in sample.items():
             
-            #filtering reviews
+            v = v.split()
+            v = [int(w) for w in v]
+            count = int(v.count(unknown_index))
+            
             if k == 'content':
-                
-                #remove the sample
-                count = int(v.count(unknown_index))
-                if count > threshold:
-                    processed_li_to_ind.pop(sample)
-                    
-                #padding, good place to add a higher order function
                 sample[k] = is_padding_required(v, content_len)
-                    
+                
+                if count > unknown_threshold_content:
+                    keep_content = False
+                
+            elif k == 'title':           
+                sample[k] = is_padding_required(v, title_len)
+                
+                if count > unknown_threshold_title:
+                    keep_title = False
+                
             elif k == 'media_type':
                 sample[k] = is_padding_required(v, media_type_len)
             elif k == 'source':
                 sample[k] = is_padding_required(v, source_len)
-            elif k == 'title':
-                sample[k] = is_padding_required(v, title_len)
-                        
-    return processed_li_to_ind
+                
+        if all([keep_content,keep_title]):
+            data.append(sample)
+               
+    return data
             
-
-data = remove_reviews_and_set_padding(processed_li_to_ind, threshold = .15) 
-processed_data = []
-
 #concatenate content, source and media-type values (appending to content) into 1 vector
-for sample in data:
+processed_data = []
+for sample in processed_li_to_ind:
+    
     title = sample['title']
-    content = sample['content']; content.extend(sample['source']); content.extend(sample['media-type'].split())
+    content = sample['content']
+    media = sample['media-type']
+    source = sample['source']
+    
+    content = ' '.join([content, media, source])
     new_sample = {'title':title, 'content':content}
     processed_data.append(new_sample)
 
-with open('processed_data.txt', 'wb') as output:
-    pickle.dump(processed_data, output)
+
+processed_data_with_padding = remove_reviews_and_set_padding(processed_data, threshold = .10) 
+
+with open('path/processed_data-aug7.txt', 'wb') as output:
+    pickle.dump(processed_data_with_padding, output)
 
 
