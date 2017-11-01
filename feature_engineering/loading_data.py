5a67549bbb93766de5de9beecbcbfddfc83af5e9
Yuri Plotkin <plotkiny@gmail.com>
2017-11-01 02:07:43 -0400

    methods fro loading/saving various data formats

---
+++ feature_engineering/loading_data.py
@@ -0,0 +1,65 @@
+#!usr/bin/env/python
+
+import json, pickle, os
+import pandas as pd
+from feature_engineering.cleaning_data import getColumnIndex
+
+def joinPathToFile(file_name, file_path=None):
+
+    """
+    Method for joining path to file
+    """
+
+    if file_path == None:
+        return file_name
+    else:
+       return os.path.join(file_path,file_name)
+
+
+def loadCSVToPandas(file, sep=';', time_col = None):
+
+    """
+    Method for loading csv data into a pandas dataframe
+    """
+
+    try:
+        if time_col:
+            col = getColumnIndex(time_col)
+            return pd.read_csv(file, sep=sep, parse_dates=[col])
+        return pd.read_csv(file, sep=sep)
+    except IOError:
+        print('The file was not found')
+
+def load_json_to_pandas(file):
+
+    """
+    Method for loading json data into a pandas dataframe. Each line of the file is a json dictionary.
+    """
+
+    try:
+        return pd.read_json(file, lines=True)
+    except IOError:
+        print('The file was not found')
+
+def read_data(file):
+    li=[]
+    with open(file) as f:
+        for line in f:
+            j = json.loads(line)
+            li.append(j)
+    return li
+
+def save_data(df,path):
+    with open(path, 'a') as f:
+        f.write(df + '\n')
+
+def save_pickle(file, data):
+    with open(file, 'wb') as output:
+        pickle.dump(data, output)
+
+def load_pickle(file):
+    with open(file, 'rb') as input:
+        return pickle.load(input)
+
+
+
